f679a78d187f1b3f7aec6a438bb1cc5625200c53
FabianHenrick <fabianhenrick@gmail.com>
2023-04-20 12:32:26 -0300

    Transactions Table and SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,36 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchFomr } from "./components/SearchForm";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <SearchFomr />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <PriceHighLight variant="income">R$ 12.000,00 </PriceHighLight>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Hamburguer</td>
+              <PriceHighLight variant="outcome">- R$ - 59,00 </PriceHighLight>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
