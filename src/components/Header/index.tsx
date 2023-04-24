2487997b502da1caa7c27ebb4876347c640512a8
Fabian Henrick <fabianhenrick@gmail.com>
2023-04-24 12:38:46 -0300

    modal pop up overlay

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from "../../assets/Logo.svg";
 import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionMondal";
 
 export function Header() {
   return (
@@ -12,14 +13,7 @@ export function Header() {
             <NewTransactionButton>Nova Transação</NewTransactionButton>
           </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay>
-              <Dialog.Content>
-                <Dialog.Title>Nova Transação</Dialog.Title>
-                <Dialog.Close />
-              </Dialog.Content>
-            </Dialog.Overlay>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
--- src/components/NewTransactionMondal/index.tsx
+++ src/components/NewTransactionMondal/index.tsx
@@ -1,3 +1,23 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { CloseButton, Content, Overlay } from "./styles";
+import { X } from "phosphor-react";
+
 export function NewTransactionModal() {
-  return <div>bla bla</div>;
+  return (
+    <Dialog.Portal>
+      <Overlay />
+      <Content>
+        <Dialog.Title>Nova Transação</Dialog.Title>
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+        <form>
+          <input type="text" placeholder="Descrição" />
+          <input type="number" placeholder="Preço" />
+          <input type="text" placeholder="Categoria" />
+          <button type="submit">Cadastrar</button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  );
 }
