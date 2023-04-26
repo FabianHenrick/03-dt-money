4028bc298969bdecba10579b9b6ff19dafbd740c
FabianHenrick <fabianhenrick@gmail.com>
2023-04-25 23:25:54 -0300

    sinc transactions table with back-end

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -6,8 +6,31 @@ import {
   TransactionsContainer,
   TransactionsTable,
 } from "./styles";
+import { useEffect, useState } from "react";
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  category: string;
+  price: number;
+  createdAt: string;
+}
 
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3333/transactions");
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  });
+
   return (
     <div>
       <Header />
@@ -16,18 +39,18 @@ export function Transactions() {
         <SearchFomr />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td>Desenvolvimento de site</td>
-              <PriceHighLight variant="income">R$ 12.000,00 </PriceHighLight>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td>Hamburguer</td>
-              <PriceHighLight variant="outcome">- R$ - 59,00 </PriceHighLight>
-              <td>Alimentação</td>
-              <td>13/04/2022</td>
-            </tr>
+            {transactions.map((transactions) => {
+              return (
+                <tr key={transactions.id}>
+                  <td>{transactions.description}</td>
+                  <PriceHighLight variant={transactions.type}>
+                    R$ {transactions.price}
+                  </PriceHighLight>
+                  <td>{transactions.category}</td>
+                  <td>{transactions.createdAt}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
